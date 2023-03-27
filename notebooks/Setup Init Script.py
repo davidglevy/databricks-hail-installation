a2078e77a1a2a25d4d9170945d1b603fb25d01f2
krisbock <krbock@microsoft.com>
2023-03-27 13:45:06 +1000

    mkdir for scripts

--- notebooks/Setup Init Script.py
+++ notebooks/Setup Init Script.py
@@ -3,6 +3,7 @@
 # MAGIC 
 # MAGIC echo "#!/bin/bash" > /tmp/hail-init.sh
 # MAGIC echo "apt-get install -y libopenblas-dev libopenblas-base liblapack3" >> /tmp/hail-init.sh
+# MAGIC mkdir -p /dbfs/scripts
 # MAGIC cp -f /tmp/hail-init.sh /dbfs/scripts/hail-init.sh
 
 # COMMAND ----------
